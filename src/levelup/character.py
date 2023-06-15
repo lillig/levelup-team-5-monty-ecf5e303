599e5a0d26247dc96ffc12e48d6b9a91fea97363
Caryn Neumann & David Fields <caryn_neumann@msn.com>
2023-06-15 19:32:25 +0000

    Added steps to Character

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,7 @@
 class Character:
     name = ""
+    steps = None
 
-    def __init__(self, character_name):
+    def __init__(self, character_name, character_steps):
         self.name = character_name
+        self.steps = character_steps
--- tests/test_character.py
+++ tests/test_character.py
@@ -4,5 +4,7 @@ from levelup.character import Character
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
+        ARBITRARY_STEPS = 0
+        testobj = Character(ARBITRARY_NAME,ARBITRARY_STEPS)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+        self.assertEqual(ARBITRARY_STEPS, testobj.steps)
