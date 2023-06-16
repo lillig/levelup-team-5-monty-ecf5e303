54605edd07cede9d958dcda6589f2ef2b0aa056d
Caryn Neumann & David Fields <caryn_neumann@msn.com>
2023-06-16 14:53:08 +0000

    Added Position Test

---
+++ tests/robot/images/test_position.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from levelup.position import Position
+
+class TestPositionWithCoordinates(TestCase):
+    def setUp(self):
+        self.COORDINATES = (5,5)
+        self.testobj = Position(self.COORDINATES)
+
+    def test_init(self):
+        self.assertEqual(self.COORDINATES, self.testobj.coordinates)
