2daad6018c304a0c0394315ba683c0f4ef8cf043
Caryn Neumann & David Fields <caryn_neumann@msn.com>
2023-06-16 16:05:37 +0000

    First Step to Controller Test

--- tests/test_game_controller.py
+++ tests/test_game_controller.py
@@ -1,8 +1,26 @@
 from unittest import TestCase
 from levelup.controller import GameController
+from levelup.character import Character
+
 
 class TestGameController(TestCase):
     def test_init(self):
         testObj = GameController()
         assert testObj.status != None
+
+    def start_game(self):
+        self.map = Map()
+        if self.character == None:
+            self.create_character(DEFAULT_CHARACTER_NAME)
+        self.character.enter_map(self.map)
+        self.status.running = True
+        self.status.current_position = (self.character.current_position.x, self.character.current_position.y)
+        self.status.move_count = 0
+
+    def create_character(self, character_name: str) -> None:
+        if character_name is not None and character_name != "":
+            self.character = Character(character_name)
+        else:
+            self.character = Character(DEFAULT_CHARACTER_NAME)
+        self.status.character_name = self.character.name
         
