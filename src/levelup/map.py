773afc0e02692f30990345f37fb3b10f163b79b6
Lillian Gray <lilliang967@gmail.com>
2023-06-16 15:45:05 +0000

    add validate position

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -5,3 +5,14 @@ class Map:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+
+    def validatePosition(self, coordinates):
+        if coordinates[1]==-1:
+            coordinates = (coordinates[0], 0)
+        elif coordinates[0]==-1:
+            coordinates = (0, coordinates[1])
+        elif coordinates[0]==10:
+            coordinates = (9, coordinates[1])
+        elif coordinates[1]==10:
+            coordinates = (coordinates[0], 9)
+        return coordinates
--- tests/test_map.py
+++ tests/test_map.py
@@ -10,3 +10,9 @@ class TestMap (TestCase):
     def test_init(self):
         self.assertEqual(self.HEIGHT, self.testobj.height)
         self.assertEqual(self.WIDTH, self.testobj.width)
+    
+    def test_validate_position(self):
+        inputs = [[(8,5), (8,5)], [(2,-1), (2,0)], [(5,10), (5,9)], [(-1,7), (0,7)], [(10,2), (9,2)]]
+        for i in inputs:
+            validCoordinates = self.testobj.validatePosition(i[0])
+            self.assertEqual(validCoordinates, i[1])
